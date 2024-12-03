d3bd24b3dab9938d3f2b27b50047d482afe554b5
dave-testaverde <davidetestaverde@gmail.com>
2024-12-03 17:52:59 +0100

    Added new route for loading vehicle details

--- lib/features/app/router/app_router.dart
+++ lib/features/app/router/app_router.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:wheely/core/di/service_locator.dart';
+import 'package:wheely/features/vehicle/domain/entities/vehicle.dart';
+import 'package:wheely/features/vehicle/presentation/pages/details_vehicle_screen.dart';
 
 import '../../vehicle/presentation/blocs/vehicle_bloc.dart';
 import '../../vehicle/presentation/pages/list_vehicle_screen.dart';
@@ -9,7 +11,22 @@ class AppRouter {
   GoRouter generateRoute() {
     return GoRouter(routes: [
       GoRoute(
+        path: '/vehicle',
+        name: 'details_vehicle',
+        builder: (context, state) {
+          Vehicle vehicle = state.extra as Vehicle;
+          return BlocProvider.value(
+            value: sl<VehicleBloc>()
+              ..add(GetDetailsVehicleEvent()),
+            child: DetailsVehicleScreen(
+              vehicle: vehicle
+            ),
+          );
+        },
+      ),
+      GoRoute(
         path: '/',
+        name: "list_vehicles",
         builder: (context, state) {
           return BlocProvider.value(
             value: sl<VehicleBloc>()
